1e549de03917cff3d2029900418e60b91d21aba1
Samuele <samuele.gugliotta23@gmail.com>
2023-07-02 17:22:43 +0200

    Login Component Comment

--- resources/views/Login/Login.jsx
+++ resources/views/Login/Login.jsx
@@ -1,20 +1,21 @@
-import './Login.css'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import './Login.css' // Importo il foglio di stile
 
-import Loading from "../../../public/assets/Loading.gif"
+import axios from 'axios' // Importo axios per effettuare le chiamate API
 
+import { useNavigate } from 'react-router-dom' // Importo useNavigate
+import { useState, useEffect } from 'react' // Importo useState e useEffect
 
-import { motion } from 'framer-motion'
+import Loading from "../../../public/assets/Loading.gif" // Importo l'icona di caricamento
+
+import { motion } from 'framer-motion' // Importo il componente per il funzionamento della transizione da pagina a pagina 
 
 
 const Login = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const navigate = useNavigate();
-    const [errorMessage, setErrorMessage] = useState('')
+    const [isLoading, setIsLoading] = useState(false) // Creo la costante utilizzando useState e impostando il valore iniziale uguale a "false"
+    const navigate = useNavigate(); // Creo la costante "navigate" richiamando useNavigate
+    const [errorMessage, setErrorMessage] = useState('') // Creo la costante utile al messaggio di errore impostando il valore iniziale uguale a una stringa vuota.
 
-    useEffect(() => {
+    useEffect(() => { // Richiamo useEffect per fare in modo che venga mostrato il relativo errore e dopo 5s questo scomparirà. Per fare ciò sono stati utilizzate le funzioni "setTimeout" e "clearTimeout"
     if (errorMessage) {
     const timeout = setTimeout(() => {
       setErrorMessage("");
@@ -27,48 +28,42 @@ const Login = () => {
 
     // Chiamata POST per effettuare il LOGIN
     const loginUser = async (event) => {
-      event.preventDefault();
-      const email = document.querySelector('input[name="email"]').value;
-      const password = document.querySelector('input[name="password"]').value;
+      event.preventDefault(); // Uso "preventDefault" per prevenire il comportomanto di default di un pulsante.
+      const email = document.querySelector('input[name="email"]').value; // Questa costante ritorna il valore presente nell'input "email"
+      const password = document.querySelector('input[name="password"]').value; // Questa costante ritorna il valore presente nell'input "password"
     
       try {
-        // Avvia il messaggio di caricamento
-        setIsLoading(true);
-    
-        // Simula un ritardo di 5 secondi
+        setIsLoading(true); // Impostando il valore di setIsLoading uguale a "true", viene avviato il processo di caricamento 
+        // Viene simulato un ritardo di 3 secondi attraverso la funzione "setTimeout"
         await new Promise((resolve) => setTimeout(resolve, 3000));
-    
-        // Esegui la chiamata API per effettuare il login
-        
+        // Viene eseguita la chiamata API per effettuare il login. Non essendo una chiamata in "local", deve essere specificato l'indirizzo corretto relativo al sito di hosting.
         await axios.post('https://dontnod-production.up.railway.app/api/login', {
-          email: email,
-          password: password
+          email: email, // Uno dei campi che deve essere riempito per effettuare il login 
+          password: password // Uno dei campi che deve essere riempito per effettuare il login 
         });
 
-        // await axios.post('http://127.0.0.1:8000/api/login', {
+        // Chiamata API ma in "local"
+        // await axios.post('http://127.0.0.1:8000/api/login', { 
         //   email: email,
         //   password: password
         // });
-        // Nascondi il messaggio di caricamento
-        setIsLoading(false);
+
+        setIsLoading(false); // Dopo i 3s viene nascosto il caricamento, in questo caso la GIF
     
-        // Reindirizza l'utente alla pagina home
-        navigate("/home");
-        localStorage.setItem('isLoggedIn', true)
-        window.location.reload();
+        navigate("/home"); // L'utente viene reindirzzato alla home
+        localStorage.setItem('isLoggedIn', true) // Nel localStorage viene impostato il valore di "isLoggedIn" uguale a "true", il che vuol dire che l'utente ha effettuato il login.
+        window.location.reload(); // Viene riaggiornata la pagina
  
-    
-      } catch (error) {
+      } catch (error) { // Viene catturato l'errore da "catch"
         if (error.response && error.response.data && error.response.data.message) {
-          setErrorMessage(error.response.data.message);
+          setErrorMessage(error.response.data.message); 
         }
-        setIsLoading(false); // Assicurati di nascondere il messaggio di caricamento in caso di errore
+        setIsLoading(false); // Il messaggio viene nascosto dopo 3s, ma in questo caso riguarda l'errore
       }
     };
     
-      
-
     return (
+      // Specifico il tipo di transizione impostando i valori di "inizial, animate e exit"
         <motion.form className='login' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}> 
             <div className='login--container'>
                 <div className='login--elements'>
@@ -76,9 +71,11 @@ const Login = () => {
                    <div className='login--field'>
                    <input type='email' placeholder='Email' name='email' className='login--field--email'></input>
                    <input type='password' placeholder='Password' name='password'></input>
+                   {/* Specifico le caratteristiche di stile del messaggio di errore */}
                    {errorMessage && <p style={{ color: `red`, fontWeight: `regular`, marginTop: `7px`, fontSize: `14px` }}>{errorMessage}</p>}
                    <button className='login--field--button' onClick={loginUser}>LOGIN</button>
                    <p className='login--field--signup'>Not registered yet? <a href='/signup'>Click here!</a></p>
+                   {/* Al richiamo del messaggio di caricamento, viene mostrata l'apposita GIF */}
                    {isLoading && <img src={Loading} className='login--loading--icon'/>}
                    </div>
                    </div>
