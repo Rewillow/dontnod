7dc46311a4d1d48ef646f027496defd5233ffd0e
Samuele <samuele.gugliotta23@gmail.com>
2023-07-02 17:33:16 +0200

    Profile Component Comment

--- resources/views/Profile/Profile.jsx
+++ resources/views/Profile/Profile.jsx
@@ -1,58 +1,51 @@
+import './Profile.css' // Importo il foglio di stile
+
 import axios from 'axios'; // Importo Axios per le chiamata API
-import { useState } from 'react'; // Importo useState che gestisce lo stato delle mie variabili
-import { useNavigate } from 'react-router-dom'; // Importo useNavigate che mi permette di reindirizzare l'utente in un'altra rotta del mio sito
+import { useState } from 'react'; // Importo useState che gestisce lo stato delle variabili
+import { useNavigate } from 'react-router-dom'; // Importo useNavigate che permette di reindirizzare l'utente in un'altra rotta del mio sito
 
+import {MdLogout, MdAccountCircle} from 'react-icons/md' // Importo le icone dalla libreria React Icons
+import {IoLogoGameControllerA} from 'react-icons/io' // Importo l'icona dalla libreria React Icons
 
-// Importo le icone
-import {MdLogout, MdDarkMode, MdAccountCircle} from 'react-icons/md'
-import {IoLogoGameControllerA} from 'react-icons/io'
+import Account from '../../js/components/Account/Account'; // Importo il componente Account
+import VideoGames from '../../js/components/VideoGames/VideoGames'; // Importo il componente Videogames
 
-// Importo i componenti dei relativi pulsanti
-import Account from '../../js/components/Account/Account';
-import VideoGames from '../../js/components/VideoGames/VideoGames';
-
-
-import './Profile.css'
-
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion' // Importo il componente per il funzionamento della transizione da pagina a pagina 
 
 const Profile = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [selectedTab, setSelectedTab] = useState('account')
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Creo la costante utilizzando useState e impostando il valore iniziale uguale a "false"
+    const [selectedTab, setSelectedTab] = useState('account') // Creo la costante utilizzando useState e impostando il valore iniziale uguale a "account", ovvero la voce iniziale che viene mostrata quando si accede alla sezione Profilo
     
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // Creo la costante utilizzando useNavigate
 
-    
-    const logOut = async (event) => {
-        event.preventDefault();
+    const logOut = async (event) => { // Gestione del LogOut
+        event.preventDefault(); // Uso "preventDefault" per prevenire il comportomanto di default di un pulsante.
         try {
-          await axios.post('https://dontnod-production.up.railway.app/api/logout');
-          // await axios.post('http://127.0.0.1:8000/api/logout');
-  
-          localStorage.setItem('isLoggedIn', 'false');
-          localStorage.removeItem('userId')
-          navigate('/login')
-          setIsLoggedIn(false)
-          window.location.reload();
+          await axios.post('https://dontnod-production.up.railway.app/api/logout'); // Viene effettuata la chiamata API
+          // await axios.post('http://127.0.0.1:8000/api/logout'); 
+          localStorage.setItem('isLoggedIn', 'false'); // Il valore di "isLoggedIn" presente nel localStorage, viene impostato uguale a "false", quindi l'utente è scollegato.
+          localStorage.removeItem('userId') // Viene rimossa la voce "userId" dal localStorage
+          navigate('/login') // L'utente viene reindirizzato alla pagina "Login"
+          setIsLoggedIn(false) // Richiamo setIsLoggedIn e il suo valore di default
+          window.location.reload(); // Viene riaggiornata la pagina
         }
-        catch(error) {
-          console.error(error)
+        catch(error) { // Gestione di eventuali errori
+          console.error(error) // Gli errori vengono mostrati nella console
         }
       }
     
-      // Creo la variabile che conterrà i rispettivi componenti
-      let content;
+      let content; // Creo la variabile che contiene ogni singola voce della sezione Profilo
       
-      // Creo la logica secondo la quale, in base al valore di "selectedTab", viene restituito il rispettivo componente
-      if(selectedTab === 'account') {
+      if(selectedTab === 'account') { // Creo la logica secondo la quale, in base al valore di "selectedTab", viene restituito il rispettivo componente
         content = <Account />
       } else if (selectedTab === 'games') {
         content = <VideoGames />
       } else {
-        content = "null";
+        content = "null"; // Se content non trova un componente correttamente inserito, restituisce "null"
       }
 
     return (
+      // Specifico il tipo di transizione impostando i valori di "inizial, animate e exit"
         <motion.div className='profile' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
         <div className='profile--text'>
             <h1>Account <span style={{ color: `#009B96`}}>Settings</span></h1>
@@ -60,11 +53,14 @@ const Profile = () => {
         </div>
         <div className='profile--container'>
             <div className='profile--sections'>
+              {/* Al click su uno degli appositi bottoni, viene mostrato il componente a esso collegato*/}
               <button  onClick={() => setSelectedTab('account')} > <MdAccountCircle className='profile--sections--icon'/> Account </button>
               <button onClick={() => setSelectedTab('games')}> <IoLogoGameControllerA className='profile--sections--icon' /> Games </button>
+              {/* Al click l'utente viene scollegato*/}
               <button onClick={logOut}> <MdLogout className='profile--sections--icon' /> Logout </button>
             </div>
         <div className='profile--sections--details'>
+        {/* Vengono mostrati selectedTab e content */}
         {selectedTab && content}
         </div>
         </div>
