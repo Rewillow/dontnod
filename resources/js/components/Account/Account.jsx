0f1ecbf63cf2ea84e9c893a041fb55a8d29ffcf4
Samuele <samuele.gugliotta23@gmail.com>
2023-07-05 12:00:00 +0200

    Account Component Comment

--- resources/js/components/Account/Account.jsx
+++ resources/js/components/Account/Account.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './Account.css'
+import './Account.css' // Importo il file di stile
+
+import { useEffect, useState } from 'react' // Importo useState e useEffect
+
+import axios from 'axios'; // Importo axios
+
 
 
 const Account = () => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null); // Creo la costante che servirà a recuperare i dati degli utenti
 
     useEffect (() => {
         const fetchUserDetails = async () => {
             try {
+                // Viene effettuata la chiamata API
                 const response = await axios.get('https://dontnod-production.up.railway.app/api/user');
                 // const response = await axios.get('http://127.0.0.1:8000/api/user');
-                setUser(response.data)
-                const userId = response.data.id;
-                localStorage.setItem('userId', userId);
+                setUser(response.data) // Imposto il valore della costante inserendo i valori di "data" recuperati dalla chiamata API
+                const userId = response.data.id; // Stabilisco il valore della costante "userId" ricavando i dati dalla chiamata API e nello specifico il valore di "id"
+                localStorage.setItem('userId', userId); // Nel localStorage imposto il valore dell'id
             } catch(error) {
                 console.error(error)
             }
@@ -21,7 +25,7 @@ const Account = () => {
         fetchUserDetails();
     }, [])
 
-    const defaultUser = {
+    const defaultUser = { // Imposto i valori di default dei vari campi
         name: '',
         email: '',
         password: ''
@@ -34,7 +38,7 @@ const Account = () => {
         <div className='account--input'>
         <form>
             <label>Username *</label>
-            <input type='text' value={(user && user.name) || defaultUser.name} readOnly></input>
+            <input type='text' value={(user && user.name) || defaultUser.name} readOnly></input> 
             <label>Email *</label>
             <input type='text' value={(user && user.email) || defaultUser.email} readOnly></input>
             <label>Password *</label>
